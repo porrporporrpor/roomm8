da777b623d506aae5ec79993179f39b49d1886bb
tiltgod <59070084@it.kmitl.ac.th>
2017-03-29 11:19:03 +0700

    Test ultra sonic

---
+++ test_ultra.ino
@@ -0,0 +1,34 @@
+const int pingPin = 13;
+  int inPin = 12;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  long duration, cm;
+  pinMode(pingPin, OUTPUT);
+
+  digitalWrite(pingPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin,LOW);
+  pinMode(inPin,INPUT);
+  duration = pulseIn(inPin, HIGH);
+
+  cm = microsecondsToCentimeters(duration);
+
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
+  
+}
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+
+
