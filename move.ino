f59df5e6601640938f273c851555a4cf8d6306db
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-03-31 20:08:36 +0700

    Display function is on

--- move.ino
+++ move.ino
@@ -1,36 +1,39 @@
-int LM1 = 4;      // left motor
-int RM1 =  5;       // right motor
+int LM1 = 7;      // left motor
+int RM1 =  6;       // right motor
 
 void motor_loop(int front, int left, int right)
 {
-  Serial.print(front);
-  Serial.print(" ");
-  Serial.print(left);
-  Serial.print(" ");
-  Serial.print(right);
-  Serial.println();
 
  //forward
  digitalWrite(LM1, HIGH);
  digitalWrite(RM1, HIGH);
- Serial.print("Forward ");
 
  //turn left
  if(front < 30 && right < 30)
  {
   digitalWrite(RM1, HIGH);
   digitalWrite(LM1, LOW);
-  Serial.println("Left");
+  Serial.print("Left Avoid:");
+  display("Left Avoid!");
   delay(600);
  } else {//turn right
  if(front < 30 && left < 30)
  {
   digitalWrite(RM1, LOW);
   digitalWrite(LM1, HIGH);
-  Serial.println("Right");
+  Serial.print("Right Avoid:");
+  display("Right Avoid!");
   delay(600);
- }
- }
- 
+} else {
+  display("Normal");
 }
+ }
 
+ Serial.print(front);
+ Serial.print(" ");
+ Serial.print(left);
+ Serial.print(" ");
+ Serial.print(right);
+ Serial.println();
+
+}
--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,33 +1,25 @@
-/*
-CODE HERE
-Array of HC-SR04 ultrasonic sensors
-*/
-
-int sensor = 0;
 int distance_front;
 int distance_left;
 int distance_right;
 
-
-
-//Sonar 1
+//Sonar Front
 int echoPin1 =12;
 int initPin1 =13;
 int distance1 =0;
 
-//Sonar 2
+//Sonar Left
 int echoPin2 =10;
 int initPin2 =11;
 int distance2 =0;
 
-//Sonar 3
+//Sonar Right
 int echoPin3 =8;
 int initPin3 =9;
 int distance3 =0;
 
 
 void setup() {
-  
+
   pinMode(initPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
   pinMode(initPin2, OUTPUT);
@@ -36,47 +28,51 @@ void setup() {
   pinMode(echoPin3, INPUT);
   pinMode(LM1, OUTPUT);
   pinMode(RM1, OUTPUT);
+
+  lcd.begin();
+  lcd.backlight();
+
   Serial.begin(9600);
-  
+
 }
 
 void loop() {
-  
+
   distance1 = getDistance(initPin1, echoPin1);
   distance_front = printDistance(1, distance1);
-  
+
   distance2 = getDistance(initPin2, echoPin2);
   distance_left = printDistance(2, distance2);
-  
+
   distance3 = getDistance(initPin3, echoPin3);
   distance_right = printDistance(3, distance3);
 
-  motor_loop(distance_front, distance_left, distance_right); 
-  
+  motor_loop(distance_front, distance_left, distance_right);
+
 }
 
 int getDistance (int initPin, int echoPin){
 
  digitalWrite(initPin, HIGH);
- delayMicroseconds(10); 
- digitalWrite(initPin, LOW); 
- unsigned long pulseTime = pulseIn(echoPin, HIGH); 
+ delayMicroseconds(10);
+ digitalWrite(initPin, LOW);
+ unsigned long pulseTime = pulseIn(echoPin, HIGH);
  int distance = pulseTime/58;
  return distance;
- 
+
 }
- 
+
  int printDistance(int id, int dist){
-  
+
 //     Serial.print(id);
     if (dist >= 250 || dist <= 0 ){
 //      Serial.println(" Out of range");
     }else
-    for (int i = 0; i <= dist; i++) { 
+    for (int i = 0; i <= dist; i++) {
 //         Serial.print("-");
     }
 //    Serial.print(dist, DEC);
 //    Serial.println(" cm");
     return dist;
-    
+
  }
