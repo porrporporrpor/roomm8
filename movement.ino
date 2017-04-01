8fec59105abb3cce6d04a35f6e2792311f1e19ab
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-04-01 15:47:50 +0700

    Explain Movement Code

--- movement.ino
+++ movement.ino
@@ -1,13 +1,13 @@
-void movement(int front, int left, int right)
-{
+void movement(int front, int left, int right) {
 
  //forward
  digitalWrite(LM1, HIGH);
  digitalWrite(RM1, HIGH);
 
-if (front < 30 && left >= 30 && right >= 30)
-{
+//Front Sensor is close to some object.
+if (front < 30 && left >= 30 && right >= 30) {
   Serial.print("Break! --> ");
+  // Which turn to use, based on distance on both side.
   if (left > right) {
     digitalWrite(RM1, HIGH);
     digitalWrite(LM1, LOW);
@@ -22,44 +22,43 @@ if (front < 30 && left >= 30 && right >= 30)
     delay(600);
   }
 } else {
-  //turn left
-  if(right < 30)
-  {
+  //Turn Left : Right Sensor is close to some object.
+  if(right < 30) {
+    // Front Sensor is also close too. Turn Left to avoid collision.
+    digitalWrite(RM1, HIGH);
+    digitalWrite(LM1, LOW);
     if (front < 30) {
-      digitalWrite(RM1, HIGH);
-      digitalWrite(LM1, LOW);
       Serial.print("Left turn : ");
       display_run_mode("Left turn!", "none");
       delay(600);
     } else {
-      digitalWrite(RM1, HIGH);
-      digitalWrite(LM1, LOW);
+      // Front Sensor isn't close. Avoid Right.
       Serial.print("Right Avoid : ");
       display_run_mode("Right Avoid!", "none");
-      delay(600);
+      delay(300);
     }
 
-  } else {//turn right
-  if(left < 30)
-  {
-    if (front < 30) {
+  } else {
+    //Turn Right : Left Sensor is close to some object.
+    if(left < 30) {
       digitalWrite(RM1, LOW);
       digitalWrite(LM1, HIGH);
-      Serial.print("Right turn : ");
-      display_run_mode("Right turn!", "none");
-      delay(600);
+      if (front < 30) {
+        // Front Sensor is also close too. Turn Right to avoid collision.
+        Serial.print("Right turn : ");
+        display_run_mode("Right turn!", "none");
+        delay(600);
+      } else {
+        // Front Sensor isn't close. Avoid Left.
+        Serial.print("Left Avoid:");
+        display_run_mode("Left Avoid!", "none");
+        delay(300);
+      }
     } else {
-      digitalWrite(RM1, LOW);
-      digitalWrite(LM1, HIGH);
-      Serial.print("Left Avoid:");
-      display_run_mode("Left Avoid!", "none");
-      delay(600);
+      display_run_mode("Normal", "none");
+      Serial.print("Normal : ");
     }
- } else {
-   display_run_mode("Normal", "none");
-   Serial.print("Normal : ");
- }
-}
+  }
 }
 
 // debug
