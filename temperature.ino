a74b20639874ba872adc572f405e3bba44516a56
Jirawan Chuapradit <59070027@it.kmitl.ac.th>
2017-04-04 23:31:18 +0700

    add linear

--- temperature.ino
+++ temperature.ino
@@ -15,3 +15,25 @@ void temperature(float temp, float arr[10]) {
   }
 
 }
+
+//int linearforcast(float arr[N])
+//{
+//    int i;
+//    float total = 0;
+//    float arr[N];
+//    //This loop for arr
+//    for (i = 0; i < N; i++)
+//    {
+//        scanf("%f", &arr[i]);
+//    }
+//
+//
+//    //This loop for sol
+//    for (i = 0; i < 3; i++)
+//    {
+//        total += arr[i];
+//    }
+//
+//    Serial.print("%f", total/N);
+//    return 0;
+//}
