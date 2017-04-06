28ffb92305a7ce73f0ffbacbb72b5bcf81bf8a71
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-04-06 21:24:40 +0700

    Buzzer active function

---
+++ beep.ino
@@ -0,0 +1,10 @@
+void too_close() {
+//  digitalWrite(beep, HIGH);
+//  delay(200);
+//  digitalWrite(beep, LOW);
+//  delay(200);
+//  digitalWrite(beep, HIGH);
+//  delay(200);
+//  digitalWrite(beep, LOW);
+}
+
