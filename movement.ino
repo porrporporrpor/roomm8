e47f66acaf6d035630e7ea0601a3b12a1208c822
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-04-08 11:19:54 +0700

    Movement fix

--- movement.ino
+++ movement.ino
@@ -1,94 +1,75 @@
 void movement(int front, int left, int right) {
 
- //forward
- digitalWrite(LM1, LOW);
- digitalWrite(LM2, HIGH);
-
- digitalWrite(RM1, LOW);
- digitalWrite(RM2, HIGH);
-
 //Front Sensor is close to some object.
 if (front < 30 && left >= 30 && right >= 30) {
-Serial.print("Break! --> ");
-// Which turn to use, based on distance on both side.
-if (left > right) {
-  digitalWrite(LM1, HIGH);
-  digitalWrite(LM2, LOW);
-  digitalWrite(RM1, LOW);
-  digitalWrite(RM2, HIGH);
-  delay(250);
-  Serial.print("Left turn : ");
-  display_run_mode("Break!", "Go Left");
-  delay(100);
-} else {
-  digitalWrite(LM1, LOW);
-  digitalWrite(LM2, HIGH);
-  digitalWrite(RM1, HIGH);
-  digitalWrite(RM2, LOW);
-  delay(250);
-  Serial.print("Right turn : ");
-  display_run_mode("Break!", "Go Right");
-  delay(100);
-}
-} else {
-//Turn Left : Right Sensor is close to some object.
-if(right < 30) {
-  // Front Sensor is also close too. Turn Left to avoid collision.
-  if (front < 30) {
+  Serial.print("Break! --> ");
+  // Which turn to use, based on distance on both side.
+  if (left > right) {
     digitalWrite(LM1, HIGH);
     digitalWrite(LM2, LOW);
     digitalWrite(RM1, LOW);
     digitalWrite(RM2, HIGH);
-    buzzer_too_close();
-    delay(250);
     Serial.print("Left turn : ");
-    display_run_mode("Left turn!", "none");
-    delay(100);
+    display_run_mode("Break!", "Go Left");
   } else {
-    // Front Sensor isn't close. Avoid Right.
     digitalWrite(LM1, LOW);
-    digitalWrite(LM2, LOW);
-    digitalWrite(RM1, LOW);
-    digitalWrite(RM2, HIGH);
-    delay(250);
-    Serial.print("Right Avoid : ");
-    display_run_mode("Right Avoid!", "none");
-    delay(100);
+    digitalWrite(LM2, HIGH);
+    digitalWrite(RM1, HIGH);
+    digitalWrite(RM2, LOW);
+    Serial.print("Right turn : ");
+    display_run_mode("Break!", "Go Right");
   }
-
-} else {
-  //Turn Right : Left Sensor is close to some object.
-  if(left < 30) {
+  } else {
+  //Turn Left : Right Sensor is close to some object.
+  if(right < 30) {
+    // Front Sensor is also close too. Turn Left to avoid collision.
     if (front < 30) {
-      // Front Sensor is also close too. Turn Right to avoid collision.
-      digitalWrite(LM1, LOW);
-      digitalWrite(LM2, HIGH);
-      digitalWrite(RM1, HIGH);
-      digitalWrite(RM2, LOW);
-      delay(250);
-      Serial.print("Right turn : ");
-      display_run_mode("Right turn!", "none");
-      delay(100);
+      digitalWrite(LM1, HIGH);
+      digitalWrite(LM2, LOW);
+      digitalWrite(RM1, LOW);
+      digitalWrite(RM2, HIGH);
+      buzzer_too_close();
+      Serial.print("Left turn : ");
+      display_run_mode("Left turn!", "none");
     } else {
-      // Front Sensor isn't close. Avoid Left.
+      // Front Sensor isn't close. Avoid Right.
+      digitalWrite(LM1, LOW);
+      digitalWrite(LM2, LOW);
+      digitalWrite(RM1, LOW);
+      digitalWrite(RM2, HIGH);
+      Serial.print("Right Avoid : ");
+      display_run_mode("Right Avoid!", "none");
+    }
+
+  } else {
+    //Turn Right : Left Sensor is close to some object.
+    if(left < 30) {
+      if (front < 30) {
+        // Front Sensor is also close too. Turn Right to avoid collision.
+        digitalWrite(LM1, LOW);
+        digitalWrite(LM2, HIGH);
+        digitalWrite(RM1, HIGH);
+        digitalWrite(RM2, LOW);
+        Serial.print("Right turn : ");
+        display_run_mode("Right turn!", "none");
+      } else {
+        // Front Sensor isn't close. Avoid Left.
+        digitalWrite(LM1, LOW);
+        digitalWrite(LM2, HIGH);
+        digitalWrite(RM1, LOW);
+        digitalWrite(RM2, LOW);
+        Serial.print("Left Avoid:");
+        display_run_mode("Left Avoid!", "none");
+      }
+    } else {
+      display_run_mode("Normal", "none");
       digitalWrite(LM1, LOW);
       digitalWrite(LM2, HIGH);
       digitalWrite(RM1, LOW);
-      digitalWrite(RM2, LOW);
-      Serial.print("Left Avoid:");
-      display_run_mode("Left Avoid!", "none");
+      digitalWrite(RM2, HIGH);
+      Serial.print("Normal : ");
     }
-  } else {
-    display_run_mode("Normal", "none");
-    digitalWrite(LM1, LOW);
-    digitalWrite(LM2, HIGH);
-    digitalWrite(RM1, LOW);
-    digitalWrite(RM1, HIGH);
-    delay(250);
-    Serial.print("Normal : ");
-    delay(100);
   }
-}
 }
 
 // debug
--- roomm8.ino
+++ roomm8.ino
@@ -24,10 +24,10 @@ int initPin3 =9;
 int distance_right =0;
 
 // Motors
-int LM1 = 7;
-int LM2 = 6;      // left motor
-int RM1 = 4;
-int RM2 = 5;       // right motor
+int RM1 = 7;
+int RM2 = 6;      // left motor
+int LM1 = 4;
+int LM2 = 5;       // right motor
 
 // temperature
 float temp = 0, arr[10];
@@ -41,47 +41,47 @@ int buzzer = 3;
 
 void setup() {
 
-  pinMode(initPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(initPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
-  pinMode(initPin3, OUTPUT);
-  pinMode(echoPin3, INPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(LM1, OUTPUT);
-  pinMode(RM1, OUTPUT);
-  pinMode(buttonPin, INPUT);
-  pinMode(LM2, OUTPUT);
-  pinMode(RM2, OUTPUT);
-  Serial.begin(9600);
-  lcd.begin();
-  lcd.backlight();
+        pinMode(initPin1, OUTPUT);
+        pinMode(echoPin1, INPUT);
+        pinMode(initPin2, OUTPUT);
+        pinMode(echoPin2, INPUT);
+        pinMode(initPin3, OUTPUT);
+        pinMode(echoPin3, INPUT);
+        pinMode(buzzer, OUTPUT);
+        pinMode(LM1, OUTPUT);
+        pinMode(RM1, OUTPUT);
+        pinMode(buttonPin, INPUT);
+        pinMode(LM2, OUTPUT);
+        pinMode(RM2, OUTPUT);
+        Serial.begin(9600);
+        lcd.begin();
+        lcd.backlight();
 
-  //Logo Output
-  lcd.print("-----ROOMM8-----");
-  lcd.setCursor(0,1);
-  lcd.print("Standby Mode");
-  delay(2000);
+        //Logo Output
+        lcd.print("-----ROOMM8-----");
+        lcd.setCursor(0,1);
+        lcd.print("Standby Mode");
+        delay(2000);
 
-  rtc.begin();
+        rtc.begin();
 }
 
 void loop() {
 
-  // Ultrasonic Get Distances
-  distance_front = getDistance(initPin1, echoPin1);
-  distance_left = getDistance(initPin2, echoPin2);
-  distance_right = getDistance(initPin3, echoPin3);
+        // Ultrasonic Get Distances
+        distance_front = getDistance(initPin1, echoPin1);
+        distance_left = getDistance(initPin2, echoPin2);
+        distance_right = getDistance(initPin3, echoPin3);
 
-  // send Distances to movement handle
-  movement(distance_front, distance_left, distance_right);
+        // send Distances to movement handle
+        movement(distance_front, distance_left, distance_right);
 
-  // temperature
-  // temp = (rtc.getTemp() - 32) / 1.8;
-  temp = rtc.getTemp();
-  temperature(temp, arr);
-  if (count<9)
-    count++;
-  // game
-  // game(buttonPin);
+        // temperature
+        // temp = (rtc.getTemp() - 32) / 1.8;
+        temp = rtc.getTemp();
+        temperature(temp, arr);
+        if (count<9)
+                count++;
+        // game
+        // game(buttonPin);
 }
